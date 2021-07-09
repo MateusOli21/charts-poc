fce305db7ccabf426afb74b065f951160db864a2
MateusOli21 <mateus.oli.abreu@hotmail.com>
2021-07-09 16:55:51 -0300

    initial commit.

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from '../styles/Home.module.css';
+
+const Home: React.FC = () => (
+  <div className={styles.container}>
+    <h1>Hello world</h1>
+  </div>
+);
+
+export default Home;
